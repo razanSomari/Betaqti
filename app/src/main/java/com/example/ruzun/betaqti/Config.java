50aa3dcc6ec6f2e80d9577151e3f07fa2eca2b09
razanSomari <54210754+razanSomari@users.noreply.github.com>
2021-01-19 18:18:39 +0300

    Update Config.java

--- app/src/main/java/com/example/ruzun/betaqti/Config.java
+++ app/src/main/java/com/example/ruzun/betaqti/Config.java
@@ -1,6 +1,6 @@
 package com.example.ruzun.betaqti;
 
 public class Config {
-    public static final String Email = "emailsendapptest@gmail.com";
-    public static final String PASSWORD = "Ae113322";
+    public static final String Email = "";
+    public static final String PASSWORD = "";
 }
